ca75488907ade0ead28e879a8ef07c93dd0f8953
ameliatjy <e0425671@u.nus.edu>
2020-06-16 03:51:46 +0800

    login and logout backend

---
+++ Firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB1oyaDAneBvtqpJJqYN_o13jWDExpRDq0",
+    authDomain: "shteady-b81ed.firebaseapp.com",
+    databaseURL: "https://shteady-b81ed.firebaseio.com",
+    projectId: "shteady-b81ed",
+    storageBucket: "shteady-b81ed.appspot.com",
+    messagingSenderId: "749591564782",
+    appId: "1:749591564782:web:73f597ecbcf1edd21dfeff",
+    measurementId: "G-CBS54V1147"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
