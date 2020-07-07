0b90d29c6dce961e7c23bc33a10abd5f17354235
ameliatjy <e0425671@u.nus.edu>
2020-07-08 00:04:52 +0800

    added menu update section

---
+++ menu/Breaktfast.js
@@ -0,0 +1,5 @@
+function submitform() {
+    document.getElementById("normalmenuform").reset();
+    document.getElementById("drinksmenuform").reset();
+    document.getElementById("othersform").reset();
+}
---
+++ menu/Dinner.js
@@ -0,0 +1,5 @@
+function submitform() {
+    document.getElementById("normalmenuform").reset();
+    document.getElementById("specialmenuform").reset();
+    document.getElementById("dessertdrinksmenuform").reset();
+}
