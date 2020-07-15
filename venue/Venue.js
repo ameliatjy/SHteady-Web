e175ff509632e951884843f1601b595d9e86d31b
ameliatjy <e0425671@u.nus.edu>
2020-07-15 22:43:02 +0800

    edited firebase branch storage

--- venue/Venue.js
+++ venue/Venue.js
@@ -121,9 +121,9 @@ function setData() {
     var starttime = document.getElementById("starttime").value; //24hour like 19:05
     var endtime = document.getElementById("endtime").value;
     var purpose = document.getElementById("purpose").value;
-    firebase.database().ref('venuebooking').push({
+    firebase.database().ref('venuebooking/' + venue).push({
         cca: cca,
-        vanue: venue,
+        venue: venue,
         startdate: startdate,
         enddate: enddate,
         starttime: starttime,
