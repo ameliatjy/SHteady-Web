f68529dd04779063439c7a6b4f8ca017cd605dce
lohwenyu <e0421622@u.nus.edu>
2020-07-22 15:48:31 +0800

    report page bug fixed

--- report/Report.js
+++ report/Report.js
@@ -15,34 +15,7 @@ $('select').on('change', function () {
 })
 
 function readTable(status) {
-    // document.getElementById('reportTable').innerHTML=''
-
-    // var report 
-    // if (status == "ALL") {
-    //     report = firebase.database().ref('report/')
-    // } else {
-    //     report = firebase.database().ref('report/').orderByChild('status').equalTo(status)
-    // }
-    
-    // report.on('value', function(snapshot) {
-
-    //     let reportValue = snapshot.val() ? snapshot.val() : {}
-    //     let keys = Object.keys(reportValue)
-
-    //     keys.map((key) => (
-    //         document.getElementById('reportTable').innerHTML+=`
-    //         <tr>
-    //             <td>${reportValue[key].location}</td>
-    //             <td>${reportValue[key].problem}</td>
-    //             <td>${reportValue[key].otherDetails}</td>
-    //             <td>${reportValue[key].reportSubmittedBy}</td>
-    //             <td>${this.getDate(reportValue[key].timeSubmitted)}</td>
-    //             <td>${reportValue[key].status}</td>                
-    //             <td>${this.getDate(reportValue[key].lastUpdatedTime)}</td>
-    //         </tr> 
-    //     `
-    //     ))
-    // })
+    document.getElementById('reportTable').innerHTML=''
     
     var descending = ''
 
