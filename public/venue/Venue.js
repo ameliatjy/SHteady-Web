9160bfd0bda2fd82b59f6e2f4e18b1eaac08ed76
ameliatjy <e0425671@u.nus.edu>
2020-07-22 21:12:40 +0800

    disallow invalid date inputs

--- public/venue/Venue.js
+++ public/venue/Venue.js
@@ -9,7 +9,6 @@ function submitform() {
     } else {
         if (confirm("Please confirm venue booking details.\nThis action is irreversible.") == true) {
             setData();
-            document.getElementById("bookingform").reset();
         }
     }
 }
@@ -25,28 +24,33 @@ function setData() {
     var endtime = document.getElementById("endtime").value;
     var purpose = document.getElementById("purpose").value;
     var newbooking = firebase.database().ref('venuebooking/' + venue).push();
-    newbooking.set({ // to display all bookings
-        submittedAt: submittedAt,
-        status: "PENDING",
-        cca: cca,
-        venue: venue,
-        startdate: startdate,
-        enddate: enddate,
-        starttime: starttime,
-        endtime: endtime,
-        purpose: purpose,
-        //need to add name of cca too
-    })
-    var newKey = newbooking.getKey();
-    var currcca = firebase.auth().currentUser.email.split('@')[0];
-    var currbookings = [];
-    var count = 0;
-    firebase.database().ref('1F0zRhHHyuRlCyc51oJNn1z0mOaNA7Egv0hx3QSCrzAg/cca/' + currcca + '/submittedbookings/').on('value', function (snapshot) {
-        currbookings = snapshot.val() ? snapshot.val() : [];
-    })
-    count = currbookings.length
-    firebase.database().ref('1F0zRhHHyuRlCyc51oJNn1z0mOaNA7Egv0hx3QSCrzAg/cca/' + currcca + '/submittedbookings/' + count).set({
-        venue: venue,
-        key: newKey,
-    });
+    if (startdate < enddate || (startdate == enddate && starttime < endtime)) {
+        newbooking.set({ // to display all bookings
+            submittedAt: submittedAt,
+            status: "PENDING",
+            cca: cca,
+            venue: venue,
+            startdate: startdate,
+            enddate: enddate,
+            starttime: starttime,
+            endtime: endtime,
+            purpose: purpose,
+            //need to add name of cca too
+        })
+        var newKey = newbooking.getKey();
+        var currcca = firebase.auth().currentUser.email.split('@')[0];
+        var currbookings = [];
+        var count = 0;
+        firebase.database().ref('1F0zRhHHyuRlCyc51oJNn1z0mOaNA7Egv0hx3QSCrzAg/cca/' + currcca + '/submittedbookings/').on('value', function (snapshot) {
+            currbookings = snapshot.val() ? snapshot.val() : [];
+        })
+        count = currbookings.length
+        firebase.database().ref('1F0zRhHHyuRlCyc51oJNn1z0mOaNA7Egv0hx3QSCrzAg/cca/' + currcca + '/submittedbookings/' + count).set({
+            venue: venue,
+            key: newKey,
+        });
+        document.getElementById("bookingform").reset();
+    } else {
+        alert('Invalid time or date fields!');
+    }
 }
